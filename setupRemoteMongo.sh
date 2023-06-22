0ba1c5e9bc5b361af95bdbd16167a59905ca78ab
Abhidruva <122786535+Abhidruva@users.noreply.github.com>
2023-06-22 22:53:54 +0530

    Add files via upload

---
+++ setupRemoteMongo.sh
@@ -0,0 +1,5 @@
+# Setup file template to upload data to MongoDB Atlas
+# mongoimport --uri "mongodb+srv://Abhidruva:Abhishek@qkart-node.6uxbwzx.mongodb.net/?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
+# mongoimport --uri "mongodb+srv://Abhidruva:Abhishek@qkart-node.6uxbwzx.mongodb.net/?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json# Setup file template to upload data to MongoDB Atlas
+mongoimport --uri "mongodb://undefined/?replicaSet=atlas-vr92uk-shard-0" --ssl --authenticationDatabase admin --username Abhidruva --password Abhishek --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb://undefined/?replicaSet=atlas-vr92uk-shard-0" --ssl --authenticationDatabase admin --username Abhidruva --password Abhishek --drop --collection products --file data/export_qkart_products.json
