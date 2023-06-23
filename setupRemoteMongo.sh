410e544707b60f4edf4fd487e27521a9c855a1c3
Abhidruva <122786535+Abhidruva@users.noreply.github.com>
2023-06-23 15:36:58 +0530

    Update setupRemoteMongo.sh
    cloud mongodb setup

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,5 +1,4 @@
 # Setup file template to upload data to MongoDB Atlas
-# mongoimport --uri "mongodb+srv://Abhidruva:Abhishek@qkart-node.6uxbwzx.mongodb.net/?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
-# mongoimport --uri "mongodb+srv://Abhidruva:Abhishek@qkart-node.6uxbwzx.mongodb.net/?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json# Setup file template to upload data to MongoDB Atlas
-mongoimport --uri "mongodb://undefined/?replicaSet=atlas-vr92uk-shard-0" --ssl --authenticationDatabase admin --username Abhidruva --password Abhishek --drop --collection users --file data/export_qkart_users.json
-mongoimport --uri "mongodb://undefined/?replicaSet=atlas-vr92uk-shard-0" --ssl --authenticationDatabase admin --username Abhidruva --password Abhishek --drop --collection products --file data/export_qkart_products.json
+# Setup file template to upload data to MongoDB Atlas
+mongoimport --uri "mongodb://ac-hhbloek-shard-00-00.6uxbwzx.mongodb.net:27017,ac-hhbloek-shard-00-01.6uxbwzx.mongodb.net:27017,ac-hhbloek-shard-00-02.6uxbwzx.mongodb.net:27017/?replicaSet=atlas-vr92uk-shard-0" --ssl --authenticationDatabase admin --username Abhidruva --password Abhishek --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb://ac-hhbloek-shard-00-00.6uxbwzx.mongodb.net:27017,ac-hhbloek-shard-00-01.6uxbwzx.mongodb.net:27017,ac-hhbloek-shard-00-02.6uxbwzx.mongodb.net:27017/?replicaSet=atlas-vr92uk-shard-0" --ssl --authenticationDatabase admin --username Abhidruva --password Abhishek --drop --collection products --file data/export_qkart_products.json
